4bb588ff0d38e152a8749174b72211f9dd53cf70
l3l3jones <111534503+l3l3jones@users.noreply.github.com>
2023-08-19 22:02:40 +0100

    setup pre-commit hooks

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import requests
 from utils import get_genres, extract_movie_data
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 #  In a production environment the api key would be stored in a environment variable on the server.
 
@@ -11,32 +13,39 @@ api_key = "eb7191390acbcface8cf637d866e443c"
 
 @app.route("/")
 def get_movies():
-    url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}"
-    response = requests.get(url)
-    data = response.json()
-
-    return render_template("movies.html", movies=data["results"])
+    # url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}"
+    # response = requests.get(url)
+    # data = response.json()
+    #
+    return render_template("movies.html")
 
 
 @app.route("/search")
 def search_movies():
     keyword = request.args.get("keyword")
+    genre = request.args.get("genre")
+    print(f"Keyword: {keyword}")
+    print(f"Genre: {genre}")
 
-    url = (
-        f"https://api.themoviedb.org/3/search/keyword?api_key={api_key}&query={keyword}"
-    )
+    if keyword:
 
-    response = requests.get(url)
-    data = response.json()
+        url = f"https://api.themoviedb.org/3/search/keyword?api_key={api_key}&query={keyword}"
 
-    keyword_id = data["results"][0]["id"]
+        response = requests.get(url)
+        data = response.json()
 
-    url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_keywords={keyword_id}"
+        keyword_id = data["results"][0]["id"]
 
-    response = requests.get(url)
-    movies = response.json()["results"]
+        url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_keywords={keyword_id}"
 
-    return render_template("movies.html", movies=movies)
+        response = requests.get(url)
+        movies = response.json()["results"]
+
+        return movies
+
+    elif genre:
+
+        return get_movies_by_genre(genre)
 
 
 @app.route("/movies/genre/<string:genre>")
@@ -44,11 +53,11 @@ def get_movies_by_genre(genre):
     url = f"https://api.themoviedb.org/3/discover/movie?api_key={api_key}&with_genres={genre}"
 
     response = requests.get(url)
-    data = response.json()
+    data = response.json()["results"]
 
-    movie_data = extract_movie_data(data)
+    # movie_data = extract_movie_data(data)
 
-    return render_template("movies.html", movies=movie_data)
+    return jsonify(data)
 
 
 @app.route("/now_playing")
